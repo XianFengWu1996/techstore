e0fda62bb5e718353d4508e2755e1642f9c4f958
Shawn <shawnwu1996@hotmail.com>
2019-09-24 12:52:10 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Switch } from 'react-router-dom'
+
+import Home from './pages/HomePage'
+import About from './pages/AboutPage'
+import Default from './pages/Default'
+import Product from './pages/ProductsPage'
+import Contact from './pages/ContactPage'
+import Cart from './pages/CartPage'
+import SingleProduct from './pages/SingleProductPage'
+
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import SideCart from './components/SideCart'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar />
+    <Sidebar />
+    <SideCart />
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/about" component={About} exact/>
+        <Route path="/contact" component={Contact} exact/>
+        <Route path="/product" component={Product} exact/>
+        <Route path="/product/:id" component={SingleProduct} />
+        <Route path="/cart" component={Cart} />
+        <Route  component={Default}/>
+      </Switch>
+
+      <Footer />
+    </>
+    
   );
 }
 
---
+++ src/context/Context.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import {linkData} from './linkdata'
+
+const ProductContext = React.createContext();
+
+class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0,
+    links: linkData
+  }
+
+  handleSidebar = () => {
+    this.setState({sidebarOpen: !this.state.sidebarOpen})
+  }
+
+  handleCart = () => {
+    this.setState({
+      cartOpen: !this.state.cartOpen
+    })
+  }
+
+  closeCart = () => {
+    this.setState({cartOpen: false})
+  }
+
+  openCart = () => {
+    this.setState({cartOpen: true})
+  }
+
+  render(){
+    return(
+    <ProductContext.Provider value={{
+      ...this.state,
+      handleSidebar: this.handleSidebar,
+      handleCart: this.handleCart,
+      openCart: this.handleCart,
+      closeCart: this.closeCart
+    }}>
+      {this.props.children}
+    </ProductContext.Provider>
+    )
+  }
+}
+
+const ProductConsumer = ProductContext.Consumer;
+
+export { ProductProvider, ProductConsumer } 
